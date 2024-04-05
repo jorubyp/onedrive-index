639804f5b5397c10bf87f4f15425ad0f00a175a7
rubyruby <jorubyp@gmail.com>
2024-04-05 09:36:24 +0100

    disable overflow db's

--- src/utils/odAuthTokenStore.ts
+++ src/utils/odAuthTokenStore.ts
@@ -4,14 +4,14 @@ import siteConfig from '../../config/site.config'
 // Persistent key-value store is provided by Redis, hosted on Upstash
 // https://vercel.com/integrations/upstash
 const kv = new Redis(process.env.REDIS_URL || '')
-const kv_overflow = new Redis(process.env.REDIS_URL_OVERFLOW || '')
+//const kv_overflow = new Redis(process.env.REDIS_URL_OVERFLOW || '')
 
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
   
   const accessToken = await kv.get(`${siteConfig.kvPrefix}:tokens:access_token`)
-    .catch(_ => kv_overflow.get(`${siteConfig.kvPrefix}:tokens:access_token`))
+    //.catch(_ => kv_overflow.get(`${siteConfig.kvPrefix}:tokens:access_token`))
   const refreshToken = await kv.get(`${siteConfig.kvPrefix}:tokens:refresh_token`)
-    .catch(_ => kv_overflow.get(`${siteConfig.kvPrefix}:tokens:refresh_token`))
+    //.catch(_ => kv_overflow.get(`${siteConfig.kvPrefix}:tokens:refresh_token`))
 
   return {
     accessToken,
@@ -29,7 +29,7 @@ export async function storeOdAuthTokens({
   refreshToken: string
 }): Promise<void> {
   await kv.set(`${siteConfig.kvPrefix}:tokens:access_token`, accessToken, 'EX', accessTokenExpiry)
-    .catch(_ => kv_overflow.set(`${siteConfig.kvPrefix}:tokens:access_token`, accessToken, 'EX', accessTokenExpiry))
+    //.catch(_ => kv_overflow.set(`${siteConfig.kvPrefix}:tokens:access_token`, accessToken, 'EX', accessTokenExpiry))
   await kv.set(`${siteConfig.kvPrefix}:tokens:refresh_token`, refreshToken)
-    .catch(_ => kv_overflow.set(`${siteConfig.kvPrefix}:tokens:refresh_token`, refreshToken))
+    //.catch(_ => kv_overflow.set(`${siteConfig.kvPrefix}:tokens:refresh_token`, refreshToken))
 }
